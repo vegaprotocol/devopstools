2349d9449541c735d4d0caeefe0b7f0be7d2f54b
daniel1302 <daniel.1302@gmail.com>
2023-04-12 13:41:08 +0000

    feat: implement chown recursive

--- tools/filesystem.go
+++ tools/filesystem.go
@@ -1,6 +1,12 @@
 package tools
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+)
 
 func FileExists(filePath string) bool {
 	_, err := os.Stat(filePath)
@@ -11,6 +17,31 @@ func FileExists(filePath string) bool {
 	return err == nil
 }
 
-func ChownR(path, username, group string) error {
-	return nil
+func ChownR(path, userName, groupName string) error {
+	userInfo, err := user.Lookup(userName)
+	if err != nil {
+		return fmt.Errorf("failed to lookup for user %s: %w", userName, err)
+	}
+
+	uID, err := strconv.Atoi(userInfo.Uid)
+	if err != nil {
+		return fmt.Errorf("failed to convert uid from string to int: %w", err)
+	}
+
+	groupInfo, err := user.LookupGroup(groupName)
+	if err != nil {
+		return fmt.Errorf("failed to lookup for group %s: %w", groupName, err)
+	}
+
+	gID, err := strconv.Atoi(groupInfo.Gid)
+	if err != nil {
+		return fmt.Errorf("failed to convert gid from string to int: %w", err)
+	}
+
+	return filepath.Walk(path, func(name string, info os.FileInfo, err error) error {
+		if err == nil {
+			err = os.Chown(name, uID, gID)
+		}
+		return err
+	})
 }
