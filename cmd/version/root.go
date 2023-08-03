21509a87eb53b30a67baeda4084cfd3616a2b935
fryd <fryderyk@vega.xyz>
2023-08-03 13:13:52 +0200

    feat: add version command (#56)

---
+++ cmd/version/root.go
@@ -0,0 +1,16 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print software version",
+	Long:  `Print software version`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("devopstools %s (%s)\n", cliVersion, cliVersionHash)
+	},
+}
