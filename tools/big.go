3fe0961a163f9707259a3bb02ef492a47f4b2cdd
daniel1302 <daniel.1302@gmail.com>
2023-10-02 16:50:34 +0200

    feat: peer review changes

--- tools/big.go
+++ tools/big.go
@@ -1,11 +1,9 @@
 package tools
 
 import (
-	"fmt"
 	"math/big"
-	"strings"
 )
 
 func AsIntStringFromFloat(amount *big.Float) string {
-	return strings.TrimRight(strings.TrimRight(fmt.Sprintf("%f", amount), "0"), ".")
+	return amount.Text('f', 0)
 }
