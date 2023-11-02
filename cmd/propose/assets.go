8b0f411ff47e1b7848272d3e8f50c9bfe4b90d51
daniel1302 <daniel.1302@gmail.com>
2023-11-02 14:44:56 +0100

    feat: vote assets

--- cmd/propose/assets.go
+++ cmd/propose/assets.go
@@ -11,6 +11,7 @@ import (
 	"code.vegaprotocol.io/vega/protos/vega"
 	commandspb "code.vegaprotocol.io/vega/protos/vega/commands/v1"
 	walletpb "code.vegaprotocol.io/vega/protos/vega/wallet/v1"
+	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 	"github.com/vegaprotocol/devopstools/governance"
 	assetsgov "github.com/vegaprotocol/devopstools/governance/assets"
@@ -27,7 +28,7 @@ var proposeAssetArgs ProposeAssetArgs
 
 // provideLPCmd represents the provideLP command
 var proposeAssetCmd = &cobra.Command{
-	Use:   "asset",
+	Use:   "assets",
 	Short: "Propose asset",
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := ProposeAssetRun(&proposeAssetArgs); err != nil {
@@ -115,12 +116,15 @@ func ProposeAssetRun(args *ProposeAssetArgs) error {
 	}
 
 	assetsByERC20 := map[string]*vega.AssetDetails{}
-	for _, asset := networkAssets {
+	for _, asset := range networkAssets {
 		erc20 := asset.GetErc20()
 		if erc20 == nil {
 			continue
 		}
+		assetsByERC20[erc20.ContractAddress] = asset
 	}
+
+	var errs *multierror.Error
 
 	for _, assetDetails := range getAssets(network.Network) {
 		logger.Info("Proposing ERC20 asset",
@@ -133,82 +137,145 @@ func ProposeAssetRun(args *ProposeAssetArgs) error {
 			zap.String("lifetime-limit", assetDetails.ERC20LifetimeLimit),
 		)
 
+		if _, assetAlreadyCreated := assetsByERC20[assetDetails.ERC20Address]; assetAlreadyCreated {
+			logger.Info("Asset with given erc20 address already created",
+				zap.String("name", assetDetails.Name),
+				zap.String("symbol", assetDetails.Symbol),
+				zap.String("erc20-address", assetDetails.ERC20Address),
+			)
+
+			continue
+		}
+
 		minClose, err := time.ParseDuration(network.NetworkParams.Params[netparams.GovernanceProposalMarketMinClose])
 		if err != nil {
-			return fmt.Errorf("failed to parse duration for %s: %w", netparams.GovernanceProposalMarketMinClose, err)
+			cError := fmt.Errorf("failed to parse duration for %s: %w", netparams.GovernanceProposalMarketMinClose, err)
+			logger.Error(
+				"failed to propose asset",
+				zap.Error(cError),
+				zap.String("name", assetDetails.Name),
+			)
+			errs = multierror.Append(cError)
+			continue
 		}
 		minEnact, err := time.ParseDuration(network.NetworkParams.Params[netparams.GovernanceProposalMarketMinEnact])
 		if err != nil {
-			return fmt.Errorf("failed to parse duration for %s: %w", netparams.GovernanceProposalMarketMinEnact, err)
+			cError := fmt.Errorf("failed to parse duration for %s: %w", netparams.GovernanceProposalMarketMinEnact, err)
+			logger.Error(
+				"failed to propose asset",
+				zap.Error(cError),
+				zap.String("name", assetDetails.Name),
+			)
+			errs = multierror.Append(cError)
+			continue
 		}
 
 		closingTime := time.Now().Add(time.Second * 20).Add(minClose)
 		enactmentTime := time.Now().Add(time.Second * 30).Add(minClose).Add(minEnact)
 
 		proposal := assetsgov.NewAssetProposal(closingTime, enactmentTime, assetDetails)
-	}
 
-	args.Logger.Info("Proposing asset")
-
-	args.Logger.Info("Proposing asset: Sending proposal")
-	walletTxReq := walletpb.SubmitTransactionRequest{
-		PubKey: network.VegaTokenWhale.PublicKey,
-		Command: &walletpb.SubmitTransactionRequest_ProposalSubmission{
-			ProposalSubmission: proposal,
-		},
-	}
-	if err := governance.SubmitTx("propose asset", network.DataNodeClient, network.VegaTokenWhale, args.Logger, &walletTxReq); err != nil {
-		return err
-	}
-
-	args.Logger.Info("Proposing asset: Waiting for proposal to be picked up on the network")
-	proposalId, err := tools.RetryReturn(6, 10*time.Second, func() (string, error) {
-		reference := proposal.Reference
-
-		res, err := network.DataNodeClient.ListGovernanceData(&v2.ListGovernanceDataRequest{
-			ProposalReference: &reference,
-		})
-		if err != nil {
-			return "", fmt.Errorf("failed to list governance data for reference %s: %w", proposal.Reference, err)
-		}
-		var proposalId string
-		for _, edge := range res.Connection.Edges {
-			if edge.Node.Proposal.Reference == reference {
-				args.Logger.Info("Found proposal", zap.String("reference", reference),
-					zap.String("status", edge.Node.Proposal.State.String()),
-					zap.Any("proposal", edge.Node.Proposal))
-				proposalId = edge.Node.Proposal.Id
-				break
-			}
-		}
-
-		if len(proposalId) < 1 {
-			return "", fmt.Errorf("got empty proposal id for the %s reference", reference)
-		}
-
-		return proposalId, nil
-	})
-
-	if err != nil {
-		return fmt.Errorf("failed to find proposal for new asset:%w", err)
-	}
-	args.Logger.Info("Proposing asset: Proposal found", zap.String("proposal-id", proposalId))
-
-	args.Logger.Info("Proposing asset: Voting on proposal", zap.String("proposal-id", proposalId))
-	voteWalletTxReq := walletpb.SubmitTransactionRequest{
-		PubKey: network.VegaTokenWhale.PublicKey,
-		Command: &walletpb.SubmitTransactionRequest_VoteSubmission{
-			VoteSubmission: &commandspb.VoteSubmission{
-				ProposalId: proposalId,
-				Value:      vega.Vote_VALUE_YES,
+		args.Logger.Info("Proposing asset: Sending proposal", zap.String("name", assetDetails.Name))
+		walletTxReq := walletpb.SubmitTransactionRequest{
+			PubKey: network.VegaTokenWhale.PublicKey,
+			Command: &walletpb.SubmitTransactionRequest_ProposalSubmission{
+				ProposalSubmission: proposal,
 			},
-		},
-	}
-	if err := governance.SubmitTx("vote on asset proposal", network.DataNodeClient, network.VegaTokenWhale, args.Logger, &voteWalletTxReq); err != nil {
-		return err
+		}
+
+		if err := governance.SubmitTx("propose asset", network.DataNodeClient, network.VegaTokenWhale, args.Logger, &walletTxReq); err != nil {
+			cError := fmt.Errorf("failed to submit propose asset transaction: %w", err)
+			logger.Error(
+				"failed to propose asset",
+				zap.Error(cError),
+				zap.String("name", assetDetails.Name),
+			)
+			errs = multierror.Append(cError)
+			continue
+		}
+
+		args.Logger.Info(
+			"Proposing asset: Waiting for proposal to be picked up on the network",
+			zap.String("name", assetDetails.Name),
+			zap.String("ref", proposal.Reference),
+		)
+		proposalId, err := tools.RetryReturn(6, 10*time.Second, func() (string, error) {
+			reference := proposal.Reference
+
+			res, err := network.DataNodeClient.ListGovernanceData(&v2.ListGovernanceDataRequest{
+				ProposalReference: &reference,
+			})
+			if err != nil {
+				return "", fmt.Errorf("failed to list governance data for reference %s: %w", proposal.Reference, err)
+			}
+			var proposalId string
+			for _, edge := range res.Connection.Edges {
+				if edge.Node.Proposal.Reference == reference {
+					args.Logger.Info("Found proposal", zap.String("reference", reference),
+						zap.String("status", edge.Node.Proposal.State.String()),
+						zap.Any("proposal", edge.Node.Proposal),
+						zap.String("name", assetDetails.Name),
+					)
+					proposalId = edge.Node.Proposal.Id
+					break
+				}
+			}
+
+			if len(proposalId) < 1 {
+				return "", fmt.Errorf("got empty proposal id for the %s reference", reference)
+			}
+
+			return proposalId, nil
+		})
+
+		if err != nil {
+			cError := fmt.Errorf("failed to find proposal for new asset:%w", err)
+
+			logger.Error(
+				"failed to propose asset",
+				zap.Error(cError),
+				zap.String("name", assetDetails.Name),
+			)
+			errs = multierror.Append(cError)
+			continue
+		}
+
+		args.Logger.Info("Proposing asset: Proposal found",
+			zap.String("proposal-id", proposalId),
+			zap.String("name", assetDetails.Name),
+		)
+
+		args.Logger.Info("Proposing asset: Voting on proposal",
+			zap.String("proposal-id", proposalId),
+			zap.String("name", assetDetails.Name),
+		)
+
+		voteWalletTxReq := walletpb.SubmitTransactionRequest{
+			PubKey: network.VegaTokenWhale.PublicKey,
+			Command: &walletpb.SubmitTransactionRequest_VoteSubmission{
+				VoteSubmission: &commandspb.VoteSubmission{
+					ProposalId: proposalId,
+					Value:      vega.Vote_VALUE_YES,
+				},
+			},
+		}
+		if err := governance.SubmitTx("vote on asset proposal", network.DataNodeClient, network.VegaTokenWhale, args.Logger, &voteWalletTxReq); err != nil {
+			cError := fmt.Errorf("failet to vote on the asset proposal: %w")
+
+			logger.Error(
+				"failed to propose asset",
+				zap.Error(cError),
+				zap.String("name", assetDetails.Name),
+			)
+			errs = multierror.Append(cError)
+			continue
+		}
+
+		args.Logger.Info("Proposing asset: Voted on proposal",
+			zap.String("proposal-id", proposalId),
+			zap.String("name", assetDetails.Name),
+		)
 	}
 
-	args.Logger.Info("Proposing asset: Voted on proposal", zap.String("proposal-id", proposalId))
-
-	return nil
+	return errs.ErrorOrNil()
 }
