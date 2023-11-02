98f106883bfd70e474e145cff57cc238474d028b
daniel1302 <daniel.1302@gmail.com>
2023-11-02 15:34:32 +0100

    feat: eth usdt mainnet market

--- governance/market/mainnet_eth_usdt.go
+++ governance/market/mainnet_eth_usdt.go
@@ -10,42 +10,41 @@ import (
 	"github.com/vegaprotocol/devopstools/tools"
 )
 
-const MainnetLinkUSDTMetadataID = "auto:mainnet_link_usdt"
+const MainnetETHUSDTMetadataID = "auto:mainnet_eth_usdt"
 
 var (
-	MainnetLinkUSDTOracleDecimalPlaces   = uint64(6)
-	MainnetLinkUSDTMarketSettlementHours = 24 * 31
-	MainnetLinkUSDTPositionDecimalPlaces = int64(1)
-	MainnetLinkUSDTDecimalPlaces         = uint64(3)
+	MainnetETHUSDTOracleDecimalPlaces   = uint64(6)
+	MainnetETHUSDTMarketSettlementHours = 24 * 31
+	MainnetETHUSDTPositionDecimalPlaces = int64(3)
+	MainnetETHUSDTDecimalPlaces         = uint64(2)
 )
 
 // settlementVegaAssetId ideally with 6 decimal places
-func NewMainnetLinkUSDT(
+func NewMainnetETHUSDT(
 	settlementVegaAssetId string,
 	oraclePubKey string,
 	closingTime time.Time,
 	enactmentTime time.Time,
 	extraMetadata []string,
 ) (*commandspb.ProposalSubmission, error) {
-
 	reference := tools.RandAlphaNumericString(40)
 
 	nowTime := time.Now()
-	settlementTime := nowTime.Add(time.Hour * (time.Duration(MainnetLinkUSDTMarketSettlementHours)))
+	settlementTime := nowTime.Add(time.Hour * (time.Duration(MainnetETHUSDTMarketSettlementHours)))
 
 	return &commandspb.ProposalSubmission{
 		Reference: reference,
 		Rationale: &vega.ProposalRationale{
-			Title: "LINK/USDT Mainnet Copy",
+			Title: "ETH/USDT Mainnet Copy",
 			Description: `# Summary
 
-This proposal requests to list LINK/USDT-231231 as a market with USDT as a settlement asset on the Vega Network as discussed in: https://community.vega.xyz/.
-
+This proposal requests to list ETH/USDT-231231 as a market with USDT as a settlement asset on the Vega Network as discussed in: https://community.vega.xyz/.
+			
 # Rationale
 
-	* LINK is among the top 20 largest Crypto asset with the highest volume and Marketcap.
-	* Given the price, 3 decimal places will be used for price due to the number of valid digits in asset price.
-	* Position decimal places will be set to 2 considering the value per contract
+	* ETH is the largest Crypto asset with the highest volume and Marketcap.
+	* Given the price, 2 decimal places will be used for price due to the number of valid digits in asset price.
+	* Position decimal places will be set to 3 considering the value per contract
 	* USDT is chosen as settlement asset due to its stability.`,
 		},
 
@@ -56,8 +55,8 @@ This proposal requests to list LINK/USDT-231231 as a market with USDT as a settl
 				NewMarket: &vega.NewMarket{
 					Changes: &vega.NewMarketConfiguration{
 						Instrument: &vega.InstrumentConfiguration{
-							Name: "LINK/USDT Mainnet Copy",
-							Code: "LINK/USDT-MAINNET",
+							Name: "ETH/USDT Mainnet Copy",
+							Code: "ETH/USDT-MAINNET",
 							Product: &vega.InstrumentConfiguration_Future{
 								Future: &vega.FutureProduct{
 									SettlementAsset: settlementVegaAssetId,
@@ -79,9 +78,9 @@ This proposal requests to list LINK/USDT-231231 as a market with USDT as a settl
 														Filters: []*datav1.Filter{
 															{
 																Key: &datav1.PropertyKey{
-																	Name:                "prices.LINK.value",
+																	Name:                "prices.ETH.value",
 																	Type:                datav1.PropertyKey_TYPE_INTEGER,
-																	NumberDecimalPlaces: &MainnetLinkUSDTOracleDecimalPlaces,
+																	NumberDecimalPlaces: &MainnetETHUSDTOracleDecimalPlaces,
 																},
 																Conditions: []*datav1.Condition{
 																	{
@@ -92,7 +91,7 @@ This proposal requests to list LINK/USDT-231231 as a market with USDT as a settl
 															},
 															{
 																Key: &datav1.PropertyKey{
-																	Name: "prices.LINK.timestamp",
+																	Name: "prices.ETH.timestamp",
 																	Type: datav1.PropertyKey_TYPE_TIMESTAMP,
 																},
 																Conditions: []*datav1.Condition{
@@ -125,22 +124,22 @@ This proposal requests to list LINK/USDT-231231 as a market with USDT as a settl
 										},
 									},
 									DataSourceSpecBinding: &vega.DataSourceSpecToFutureBinding{
-										SettlementDataProperty:     "prices.LINK.value",
+										SettlementDataProperty:     "prices.ETH.value",
 										TradingTerminationProperty: "vegaprotocol.builtin.timestamp",
 									},
 								},
 							},
 						},
-						DecimalPlaces: MainnetLinkUSDTDecimalPlaces,
+						DecimalPlaces: MainnetETHUSDTDecimalPlaces,
 						Metadata: append([]string{
-							"base:LINK",
+							"base:ETH",
 							"quote:USDT",
 							"class:fx/crypto",
 							"quarterly",
 							"sector:defi",
 							fmt.Sprintf("enactment:%s", enactmentTime.Format(time.RFC3339)),
 							fmt.Sprintf("settlement:%s", settlementTime.Format(time.RFC3339)),
-							MainnetLinkUSDTMetadataID,
+							MainnetETHUSDTMetadataID,
 						}, extraMetadata...),
 						PriceMonitoringParameters: &vega.PriceMonitoringParameters{
 							Triggers: []*vega.PriceMonitoringTrigger{
@@ -181,7 +180,7 @@ This proposal requests to list LINK/USDT-231231 as a market with USDT as a settl
 								},
 							},
 						},
-						PositionDecimalPlaces:   MainnetLinkUSDTPositionDecimalPlaces,
+						PositionDecimalPlaces:   MainnetETHUSDTPositionDecimalPlaces,
 						LpPriceRange:            &[]string{"0.05"}[0],
 						LiquiditySlaParameters:  nil,
 						LinearSlippageFactor:    "0.001",
