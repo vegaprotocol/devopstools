0d33b07dbfdb3595d04aa1bcce7ba82c34aff680
Valentin Trinque <val.trinque@gmail.com>
2024-03-13 19:45:05 +0100

    fix: Remove unused commands

--- cmd/topup/root.go
+++ cmd/topup/root.go
@@ -2,6 +2,7 @@ package topup
 
 import (
 	"github.com/spf13/cobra"
+
 	rootCmd "github.com/vegaprotocol/devopstools/cmd"
 )
 
@@ -11,7 +12,6 @@ type TopUpArgs struct {
 
 var topUpArgs TopUpArgs
 
-// Root Command for OPS
 var TopUpCmd = &cobra.Command{
 	Use:   "topup",
 	Short: "Deposit ERC20 assets to vega pub keys",
