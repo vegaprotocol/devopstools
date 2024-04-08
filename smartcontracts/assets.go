2914e91a267a753238e355ea9d74ec8e7db00156
Valentin Trinque <val.trinque@gmail.com>
2024-04-08 18:04:30 +0200

    chore: Remove unused ERC20 token contracts

--- smartcontracts/assets.go
+++ smartcontracts/assets.go
@@ -12,14 +12,13 @@ type VegaAsset struct {
 	Name       string
 	HexAddress string
 	EthNetwork types.ETHNetwork
-	Version    erc20token.Version
 }
 
 func (m *Manager) GetAssetWithName(name string) (*erc20token.ERC20Token, error) {
 	name = strings.ToLower(name)
 	for _, asset := range m.assets {
 		if strings.ToLower(asset.Name) == name {
-			return m.GetAsset(asset.HexAddress, asset.EthNetwork, asset.Version)
+			return m.GetAsset(asset.HexAddress, asset.EthNetwork)
 		}
 	}
 	return nil, fmt.Errorf("there is not token with name %s", name)
@@ -29,20 +28,16 @@ func (m *Manager) GetAssetWithAddress(hexAddress string) (*erc20token.ERC20Token
 	hexAddress = strings.ToLower(hexAddress)
 	for _, asset := range m.assets {
 		if strings.ToLower(asset.HexAddress) == hexAddress {
-			return m.GetAsset(asset.HexAddress, asset.EthNetwork, asset.Version)
+			return m.GetAsset(asset.HexAddress, asset.EthNetwork)
 		}
 	}
 	return nil, fmt.Errorf("there is not token with address %s", hexAddress)
 }
 
-func (m *Manager) GetAsset(
-	hexAddress string,
-	ethNetwork types.ETHNetwork,
-	version erc20token.Version,
-) (*erc20token.ERC20Token, error) {
+func (m *Manager) GetAsset(hexAddress string, ethNetwork types.ETHNetwork) (*erc20token.ERC20Token, error) {
 	client, err := m.ethClientManager.GetEthClient(ethNetwork)
 	if err != nil {
 		return nil, err
 	}
-	return erc20token.NewERC20Token(client, hexAddress, version)
+	return erc20token.NewERC20Token(client, hexAddress)
 }
--- smartcontracts/erc20token/mint.go
+++ smartcontracts/erc20token/mint.go
@@ -1,7 +1,6 @@
 package erc20token
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -10,14 +9,5 @@ import (
 )
 
 func (t *ERC20Token) Mint(opts *bind.TransactOpts, to common.Address, amount *big.Int) (*types.Transaction, error) {
-	switch t.Version {
-	case Base:
-		return t.v_TokenBase.Mint(opts, to, amount)
-	case Old:
-		return t.v_TokenOld.Issue(opts, to, amount)
-	case Other, Minimal:
-		return nil, nil
-	default:
-		return nil, fmt.Errorf("not implemented for token %s", t.Version)
-	}
+	return t.client.Mint(opts, to, amount)
 }
